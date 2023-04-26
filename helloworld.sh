95dde16100005f2d305e492f9840767dff06d11c
Robert C <robbyarng@yahoo.com>
2023-04-26 06:09:54 +0000

    Added helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Robert C
+# Date of latest revision:      04/25/2023
+# Purpose:                      Create a script that prints a string to the console
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+var="Hello World"
+ 
+echo "$var"
+
+
+# End
