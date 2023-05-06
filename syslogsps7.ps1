0c87e1aa8f5710cb9a2fdb48e1a6f08bd77bee1d
Robert C <robbyarng@yahoo.com>
2023-05-06 03:11:53 +0000

    re-saved powershell scripts

---
+++ syslogsps7.ps1
@@ -0,0 +1,35 @@
+<#  
+Create a Powershell script that performs these operations on separate lines.
+The overall script is not designed to operate holistically, but rather act 
+as a reference for how to execute various interesting operations with the 
+process family of Powershell commandlets. Clearly indicate with 
+comments each component below.
+---------------------------------------------------
+Remember to follow this class’s commenting requirements on all scripts.
+---------------------------------------------------
+1: Output all events from the System event log that occurred in the last 
+24 hours to a file on your desktop named last_24.txt.
+2: Output all “error” type events from the System event log to a file on 
+your desktop named errors.txt.
+3: Print to the screen all events with ID of 16 from the System event log.
+4: Print to the screen the most recent 20 entries from the System event 
+log.’
+5: Print to the screen all sources of the 500 most recent entries in the 
+System event log. Ensure that the full lines are displayed (get rid of 
+the … and show the entire text).  
+#>
+
+# Output all events from the System event log that occurred in the last 24 hours to a file on your desktop named last_24.txt.
+Get-WinEvent -LogName System -FilterHashtable @{LogName='System'; StartTime=(Get-Date).AddDays(-1)} | Out-File "$env:userprofile\Desktop\last_24.txt"
+
+# Output all “error” type events from the System event log to a file on your desktop named errors.txt.
+Get-WinEvent -LogName System -FilterHashTable @{LogName='System'; Level=2} | Out-File "$env:userprofile\Desktop\errors.txt"
+
+# Print to the screen all events with ID of 16 from the System event log.
+Get-WinEvent -LogName System -FilterHashtable @{LogName='System'; Id=16}
+
+# Print to the screen the most recent 20 entries from the System event log.
+Get-WinEvent -LogName System -FilterHashtable @{LogName='System'; Top=20}
+
+# Print to the screen all sources of the 500 most recent entries in the System event log. Ensure that the full lines are displayed (get rid of the … and show the entire text).
+Get-WinEvent -LogName System -FilterHashtable @{LogName='System'; Top=500} | Select-Object -ExpandProperty ProviderName | Sort-Object -Unique
